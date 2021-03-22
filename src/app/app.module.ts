89c193abe3de86fef4f43444e2cc4a7c5ce0666a
kyasaschandra <45892772+kyasaschandra@users.noreply.github.com>
2021-03-23 03:00:54 +0530

    New v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { LoginService } from './login.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule} from "@angular/forms"
+
+import { BatteryLevel } from './battery.directive';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DeviceItemFormComponent } from './device-item-form/device-item-form.component';
+import { DeviceItemComponent } from './device-item/device-item.component';
+import { DeviceItemListComponent } from './device-item-list/device-item-list.component';
+import { lookupListToken, lookupLists } from './providers'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent, 
+    DeviceItemFormComponent, 
+    DeviceItemComponent, 
+    DeviceItemListComponent,
+    BatteryLevel,
+    LoginService
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ {provide: lookupListToken,useValue: lookupLists} ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
